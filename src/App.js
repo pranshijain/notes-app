ea5820d0268d8105857ef9c21d62aba760661114
pranshijain <pranshi.jain02@gmail.com>
2023-07-30 16:01:45 +0530

    notes app version 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { TrashIcon, PlusIcon } from '@heroicons/react/24/solid'
+// import note from './Note'
 
 function App() {
+  const [notes, setNotes] = React.useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+  function add() {
+    setNotes((prevNote) =>  [...prevNote, ``]);
+  }
+
+  // notes  [{note: 'pranshu', date: 'yz'}, {note: '', date: ''}, {note: 'hello', date:'22/11/23'}]
+  function handleChange(event, i) {
+    setNotes((prevNotes) => {
+      prevNotes[i] = event.target.value;
+
+      return [...prevNotes];
+    });
+  }
+  // const handleDateChange = (event, i) => {
+  //   setNotes((prevNotes) => {
+  //     prevNotes[i].date = event.target.value;
+  //     return[...prevNotes];
+  //   });
+  // };
+
+  function handleDelete(i) {
+    console.log("i is: ", i);
+    const isConfirm = window.confirm("Are you sure to delete?");
+
+    if (isConfirm) {
+      setNotes(
+        notes.filter((note, idx) => {
+          return idx !== i;
+        })
+      );
+    } else {
+      console.log("Delete operation cancelled");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_border">
+      <div className="titleCircle">
+        <h1 className="title">NOTES✎</h1>
+        <div className="circle"></div>
+      </div>
+
+      <h3 className="add" onClick={add}>
+        {" "}
+        Add 
+        <PlusIcon width={20} height={20} />
+        {" "}
+      </h3>
+      <ul className="mainBox">
+        {notes.map((note, i) => {
+          console.log("hello");
+          return (
+            <li className="container" key={i}>
+              <p className="notebook">📝</p>
+              <input
+                className="input"
+                value={note}
+                onChange={(event) => handleChange(event, i)}
+                placeholder={"Enter Note here"}
+              />
+              {/* <input
+                className="noteDate"
+                type="date"
+                value={note.date}
+                onChange={(event) => handleDateChange(event, i)}
+              /> */}
+
+              <button className="delete" onClick={() => handleDelete(i)}>
+                <TrashIcon width={24} height={24} />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
